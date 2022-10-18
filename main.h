72e477236b4b171e6b560faa4719367e11f8e31a
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-10-18 04:53:52 -0700

    tasks'

--- main.h
+++ main.h
@@ -1,70 +1,38 @@
-#ifndef _PRINTF_H_
-#define _PRINTF_H_
+#ifndef _PRINTF_
+#define _PRINTF_
 
 #include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define OUTPUT_BUF_SIZE 1024
-#define BUF_FLUSH -1
+/**
+ * struct convert - defines a structure for symbols and functions
+ * @sym: The operator
+ * f: The function associated
+ */
 
-#define FIELD_BUF_SIZE 50
+struct cvrt
+{
+	char *symbol;
+	int (*f)(va_list);
+};
+typedef struct cvrt _cvrt;
 
-#define NULL_STRING "(null)"
-
-#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
-int _puts(char *str);
-int _putchar(int c);
-
-/* print_functions.c module */
-int print_char(va_list ap, params_t *params);
-int print_int(va_list ap, params_t *params);
-int print_string(va_list ap, params_t *params);
-int print_percent(va_list ap, params_t *params);
-int print_S(va_list ap, params_t *params);
-
-/* number.c module */
-char *convert(long int num, int base, int flags, params_t *params);
-int print_unsigned(va_list ap, params_t *params);
-int print_address(va_list ap, params_t *params);
-
-/* specifier.c module */
-int (*get_specifier(char *s))(va_list ap, params_t *params);
-int get_print_func(char *s, va_list ap, params_t *params);
-int get_flag(char *s, params_t *params);
-int get_modifier(char *s, params_t *params);
-char *get_width(char *s, params_t *params, va_list ap);
-
-/* convert_number.c module */
-int print_hex(va_list ap, params_t *params);
-int print_HEX(va_list ap, params_t *params);
-int print_binary(va_list ap, params_t *params);
-int print_octal(va_list ap, params_t *params);
-
-/* simple_printers.c module */
-int print_from_to(char *start, char *stop, char *except);
-int print_rev(va_list ap, params_t *params);
-int print_rot13(va_list ap, params_t *params);
-
-/* print_number.c module */
-int _isdigit(int c);
-int _strlen(char *s);
-int print_number(char *str, params_t *params);
-int print_number_right_shift(char *str, params_t *params);
-int print_number_left_shift(char *str, params_t *params);
-
-/* params.c module */
-void init_params(params_t *params, va_list ap);
-
-/* string_fields.c modoule */
-char *get_precision(char *p, params_t *params, va_list ap);
-
-/* _prinf.c module */
+int _putchar(char);
 int _printf(const char *format, ...);
+int parser(const char *format, _cvrt f_list[], va_list arg_list);
+int p_char(va_list);
+int p_percent(va_list);
+int p_num(va_list);
+int p_int(va_list);
+int p_str(va_list);
+int rot13(va_list);
+int unsigned_int(va_list);
 
-#endif /*_MAIN_H_*/
+unsigned int base_len(unsigned int, int);
+char *rev_str(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int p_u_num(unsigned int);
+
+#endif
