caf457ef8e1354aeb812b8de5517e125be0a7389
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-10-19 14:12:33 -0700

    Task 0

--- 1-printf.c
+++ 1-printf.c
@@ -6,30 +6,41 @@
  */
 int _printf(const char *format, ...)
 {
-	int p_chars;
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
 
-	_cvrt f_list[] = {
-		{"c", p_char},
-		{"s", p_str},
-		{"%", p_percent},
-		{"d", p_int},
-		{"i", p_int},
-		{"r", p_str_rev},
-		{"R", rot13},
-		{"b", p_binary},
-		{"u", unsigned_int},
-		{"o", p_octal},
-		{"x", p_hex},
-		{"X", p_heX},
+	params_t params = PARAMS_INIT;
 
-		{NULL, NULL}
-	};
-	va_list arg_list;
+	va_start(ap, format);
 
-	if (format == NULL)
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	va_start(arg_list, format);
-	p_chars = parser(format, f_list, arg_list);
-	va_end(arg_list);
-	return (p_chars);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(&params, ap);
+		if (*p != '%')
+		{
+			sum += _putchar(*p);
+			continue;
+		}
+		start = p;
+		p++;
+		while (get_flag(p, &params))
+			p++;
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+					params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, ap, &params);
+	}
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
 }
